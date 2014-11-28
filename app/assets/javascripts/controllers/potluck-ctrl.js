3906f27e8971c4f8cdcf0bed94391be27c802d15
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-11-27 22:57:16 -0600

    add cancel button to rsvp and potluck views

--- app/assets/javascripts/controllers/potluck-ctrl.js
+++ app/assets/javascripts/controllers/potluck-ctrl.js
@@ -3,22 +3,25 @@ app.controller('PotluckCtrl', ['$scope', '$location', 'User',
     $scope.response = true;
     $scope.user = User.currentUser.get();
     $scope.user.$promise.then(function(data){
-      $scope.potluckStatus = data.potluck;
       if (data.potluck !== null) {
-        $scope.response = false;
         $scope.update = true;
+        $scope.response = false;
       }
     });
+    $scope.allUsers = User.allUsers.query();
     $scope.seeForm = function() {
       $scope.response = true;
       $scope.update = false;
     };
-    $scope.allUsers = User.allUsers.query();
+    $scope.cancel = function() {
+      $scope.update = true;
+      $scope.response = false;
+    }
     $scope.updatePotluck = function(food){
       User.allUsers.update(food).$promise.then(function(){
         $scope.allUsers = User.allUsers.query();
-        $scope.response = false;
         $scope.update = true;
+        $scope.response = false;
       });      
     }
   }]);
--- app/assets/javascripts/controllers/rsvp-ctrl.js
+++ app/assets/javascripts/controllers/rsvp-ctrl.js
@@ -3,22 +3,25 @@ app.controller('RSVPCtrl', ['$scope', '$location', 'User',
     $scope.response = true;
     $scope.user = User.currentUser.get();
     $scope.user.$promise.then(function(data){
-      $scope.rsvpStatus = data.rsvp;
       if (data.rsp !== null) {
-        $scope.response = false;
         $scope.update = true;
+        $scope.response = false;
       }
     });
+    $scope.allUsers = User.allUsers.query();
     $scope.seeForm = function() {
       $scope.response = true;
       $scope.update = false;
     }
-    $scope.allUsers = User.allUsers.query();
+    $scope.cancel = function() {
+      $scope.update = true;
+      $scope.response = false;
+    }
     $scope.updateRSVP = function(status){
       User.allUsers.update(status).$promise.then(function(){
         $scope.allUsers = User.allUsers.query();
-        $scope.response = false;
         $scope.update = true;
+        $scope.response = false;
       });      
     }
   }]);
