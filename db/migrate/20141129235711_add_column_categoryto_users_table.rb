16a1236a1623da820af56e423dc23c6d54276ec0
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-11-29 18:16:01 -0600

    add category column to users table and made columns in potluck table sortable

---
+++ db/migrate/20141129235711_add_column_categoryto_users_table.rb
@@ -0,0 +1,5 @@
+class AddColumnCategorytoUsersTable < ActiveRecord::Migration
+  def change
+    add_column :users, :category, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141128001714) do
+ActiveRecord::Schema.define(version: 20141129235711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20141128001714) do
     t.datetime "updated_at"
     t.string   "rsvp"
     t.string   "potluck"
+    t.string   "category"
   end
 
 end
